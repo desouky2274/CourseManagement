8f674250befebf5dde8f329870a96ae3d64684de
OmarAyman415 <o.ayman415@gmail.com>
2021-03-14 15:20:24 +0200

    used mouse Exit

--- src/com/company/gui/addStudent.java
+++ src/com/company/gui/addStudent.java
@@ -83,6 +83,8 @@ public class addStudent extends javax.swing.JFrame {
         Course6 = new javax.swing.JComboBox<>();
         Course7 = new javax.swing.JComboBox<>();
 
+
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowOpened(java.awt.event.WindowEvent evt) {
